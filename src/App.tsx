f98c924ae5fb05faa2b1a028bb4249123c24979e
Lucas <neexdeveloper@gmail.com>
2021-06-21 09:55:31 -0300

    chore: Lesson 01

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button'; 
+
+function App() {
+  return (
+    <>
+      <Button />
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+export default App;
